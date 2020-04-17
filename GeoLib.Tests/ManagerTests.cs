03a434f75876feae509ba457101181c60e1ff6b1
ignacio <ignaciomenchaca666@gmail.com>
2020-04-17 17:03:09 -0500

    initial commit

---
+++ GeoLib.Tests/ManagerTests.cs
@@ -0,0 +1,31 @@
+﻿using GeoLib.Data;
+using GeoLib.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GeoLib.Tests
+{
+    [TestClass]
+    public class ManagerTests
+    {
+        [TestMethod]
+        public void test_zip_code_retrieval()
+        {
+            var mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+            var zipCode = new ZipCode
+            {
+                City = "LINCOLN PARK",
+                State = new State {Abbreviation = "NJ"},
+                Zip = "07035"
+            };
+
+            mockZipCodeRepository.Setup(repository => repository.GetByZip("07035")).Returns(zipCode);
+            var geoService = new GeoManager(mockZipCodeRepository.Object);
+            var data = geoService.GetZipInfo("07035");
+            
+            Assert.IsTrue(data.City.ToUpper() == "LINCOLN PARK");
+            Assert.IsTrue(data.State == "NJ");
+        }
+    }
+}
